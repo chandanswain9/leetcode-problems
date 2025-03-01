a53b3f258ff1f3689d137e87d61d927978677a44
Chandan Swain <chandanswain8788@gmail.com>
2025-03-01 22:53:17 +0530

    Time: 5 ms (58.23%) | Memory: 18 MB (17.02%) - LeetSync

---
+++ LeetcodeProblems/445-add-two-numbers-ii/add-two-numbers-ii.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head):
+        prev = None
+        while head:
+            next_node = head.next
+            head.next = prev
+            prev = head
+            head = next_node
+        return prev
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        l1 = self.reverseList(l1)
+        l2 = self.reverseList(l2)
+        
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            
+            total = val1 + val2 + carry
+            carry = total // 10
+            curr.next = ListNode(total % 10)
+            curr = curr.next
+            
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+        
+        return self.reverseList(dummy.next)
