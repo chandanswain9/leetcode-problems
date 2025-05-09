10cc03d8c96214e4fe6e55d98c754bde44994925
Chandan Swain <chandanswain8788@gmail.com>
2025-05-09 14:09:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (34.65%) - LeetSync

--- LeetcodeProblems/2128-reverse-prefix-of-word/reverse-prefix-of-word.java
+++ LeetcodeProblems/2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -6,9 +6,8 @@ class Solution {
         StringBuilder str = new StringBuilder("");
         int idx = word.indexOf(ch);
         int temp = idx;
-        while(idx >= 0) {
-            str.append(word.charAt(idx));
-            idx--;
+        for(int i =idx; i >= 0; i--){
+            str.append(word.charAt(i));
         }
         str.append(word.substring(temp+1));
         return str.toString();
