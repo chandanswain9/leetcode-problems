c4e340fe67e834b815db8950b2e8c4d005609b1b
Chandan Swain <chandanswain8788@gmail.com>
2026-03-25 23:37:03 +0530

    Time: 4 ms (97.2%), Space: 160.4 MB (89.51%) - LeetHub

---
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -0,0 +1,52 @@
+class Solution {
+    long rows[], cols[]; 
+
+    public boolean canPartitionGrid(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        rows = new long[m];
+        cols = new long[n];
+
+        long sum = getSum(grid);
+
+        // Check row partition
+        long currsum = rows[0];
+        for(int i = 1; i < m; i++) {
+            if(currsum == sum - currsum) return true;
+            currsum += rows[i];
+        }
+
+        // Check column partition
+        currsum = cols[0];
+        for(int j = 1; j < n; j++) {
+            if(currsum == sum - currsum) return true;
+            currsum += cols[j];
+        }
+
+        return false;
+    }
+
+    long getSum(int[][] grid) {
+        long sum = 0;
+
+        // Row sums
+        for(int i = 0; i < grid.length; i++) {
+            long rowsum = 0;
+            for(int j = 0; j < grid[0].length; j++) {
+                sum += grid[i][j];
+                rowsum += grid[i][j];
+            }
+            rows[i] = rowsum;
+        }
+
+        // Column sums
+        for(int j = 0; j < grid[0].length; j++) {
+            long colsum = 0;
+            for(int i = 0; i < grid.length; i++) {
+                colsum += grid[i][j];
+            }
+            cols[j] = colsum;
+        }
+
+        return sum;
+    }
+}
